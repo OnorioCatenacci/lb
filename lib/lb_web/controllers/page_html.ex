a226a995b6a44d1e8d60bb997243c5ba933b3767
Onorio Catenacci <catenacci@ieee.org>
2024-06-17 15:15:25 -0400

    Modified to show MiniXWord in minutes and seconds
    I modified the display of the average for the MiniXWord to display in
    minutes and seconds to make it a bit easier to read.

--- lib/lb_web/controllers/page_html.ex
+++ lib/lb_web/controllers/page_html.ex
@@ -2,4 +2,9 @@ defmodule LbWeb.PageHTML do
   use LbWeb, :html
 
   embed_templates "page_html/*"
+  def convert_number_to_timespan(number) do
+    {minutes, seconds} = Decimal.div_rem(number, 60)
+    pluralize = if Decimal.gt?(minutes, 1), do: "mins", else: "min"
+    "#{Decimal.to_string(minutes)} #{pluralize}, #{Decimal.to_string(seconds)} secs"
+  end
 end
